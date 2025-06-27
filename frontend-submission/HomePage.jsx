d3931e640b94d343b75cdcabbcd36a6100aee9f5
Ayush3939 <ayushyadav6113@gmail.com>
2025-06-27 15:40:58 +0530

    Create HomePage.jsx

---
+++ frontend-submission/HomePage.jsx
@@ -0,0 +1,10 @@
+import UrlShortener from '../components/UrlShortener';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>URL Shortener</h1>
+      <UrlShortener />
+    </div>
+  );
+}
